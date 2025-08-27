5fbdb5d37a208373bd47c585a1e20982df6ef184
kaidenvlr <kaidenvlr@gmail.com>
2025-08-27 17:19:21 +0500

    import routing fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from '@/prisma/prisma.module';
+import { UsersModule } from '@/users/users.module';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
     imports: [PrismaModule, UsersModule, AuthModule],
